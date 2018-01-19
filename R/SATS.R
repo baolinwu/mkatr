874ba3ebf28fbcc1f844440f18f54e01c0e40567
Baolin Wu <baolin@umn.edu>
2018-01-18 22:18:22 -0600

    update

--- R/SATS.R
+++ R/SATS.R
@@ -1,26 +1,26 @@
 #' SNP-set association tests using GWAS Summary data
 #'
-#' Compute p-values for the SNP-set tests using GWAS Z-statistics: variance components test (VC), sum test (ST), and adaptive test (AT).
-#' With less than two values of \eqn{\rho}'s, it outputs only p-values for VC and ST.
+#' Compute p-values for the SNP-set tests using GWAS Z-statistics: squared sum test (S2T), sum test (ST), and adaptive test (AT).
+#' With less than two values of \eqn{\rho}'s, it outputs only p-values for S2T and ST.
 #' @param  Z GWAS summary Z-statistics for a set of SNPs
 #' @param  R SNP pairwise LD matrix
 #' @param  W SNP weights. Default to equal weights
 #' @param  rho weights for the ST
 #' @return 
 #' \describe{
-#'   \item{p.value}{ p-values for AT, VC, and ST }
+#'   \item{p.value}{ p-values for AT, S2T, and ST }
 #'   \item{pval}{ the list of all p-values }
 #'   \item{rho.est}{ estimated optimal \eqn{\rho} value }
 #' }
-#' @keywords SATS
+#' @keywords sats
 #' @export
 #' @references
 #' Guo,B., Wu,B.(2017). Statistical methods to detect novel genetic variants using publicly available GWAS summary data. tech report.
 #' @examples
 #' R = cor(matrix(rnorm(500),100,5)*sqrt(0.8)+rnorm(100)*sqrt(0.2))
 #' Z = rnorm(5) + 0:4
-#' SATS(Z,R)
-SATS <- function(Z,R,W=NULL, rho=c((0:5/10)^2,0.5,1)){
+#' sats(Z,R)
+sats <- function(Z,R,W=NULL, rho=c((0:5/10)^2,0.5,1)){
   M = length(Z)
   if(is.null(W)) W = rep(1,M)
   ##
@@ -32,13 +32,13 @@ SATS <- function(Z,R,W=NULL, rho=c((0:5/10)^2,0.5,1)){
   ## ST
   Qb = sum(Zw)^2
   pvalb = pchisq(Qb/R1, 1,lower=FALSE)
-  ## VC 
+  ## S2T
   Qv = sum(Zw^2)
   pvalv = KATpval(Qv,lamR)
   ## AT
   L = length(rho)
   if(L<=2){
-    return(list(p.value=c(A=NULL, V=pvalv, S=pvalb), pval=pval) )
+    return(list(p.value=c(A=NULL, S2=pvalv, S=pvalb), pval=pval) )
   } 
   L1 = L-1; rho1 = rho[-L]
   Qw = (1-rho)*Qv + rho*Qb
@@ -71,7 +71,7 @@ SATS <- function(Z,R,W=NULL, rho=c((0:5/10)^2,0.5,1)){
     p.value = try({ minP + integrate(katint, 0,q1, abs.tol=minP*prec)$val }, silent=TRUE)
   }
   p.value = min(p.value,minP*L)
-  return(list(p.value=c(A=p.value, V=pvalv, S=pvalb), pval=pval, rho.est=rho[which.min(pval)]) )
+  return(list(p.value=c(A=p.value, S2=pvalv, S=pvalb), pval=pval, rho.est=rho[which.min(pval)]) )
 }
 
 
